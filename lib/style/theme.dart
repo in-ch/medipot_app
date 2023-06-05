7248a8a0ae4e39503aa0b6a2f91469c16e591724
in-ch <sxin2949@naver.com>
2023-06-05 23:56:45 +0900

    style(style): add style theme

---
+++ lib/style/theme.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromRGBO(45, 66, 185, 1),
+  primary: const Color.fromRGBO(45, 66, 185, 1),
+  secondary: const Color.fromRGBO(45, 66, 185, 1),
+  background: const Color.fromARGB(31, 250, 250, 250),
+);
+
+final appTheme = ThemeData(
+  // 텍스트 스타일 설정
+  colorScheme: colorScheme,
+  textTheme: const TextTheme(
+    headlineLarge: TextStyle(
+      fontSize: 26,
+      fontWeight: FontWeight.bold,
+      color: Colors.black87,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      color: Colors.black87,
+    ),
+    headlineSmall: TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+      color: Colors.black87,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.black87,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 18,
+      color: Colors.black87,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 16,
+      color: Colors.black87,
+    ),
+  ),
+  // 버튼 테마 스타일 설정
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: ButtonStyle(
+      // 버튼 배경색 설정
+      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+        (Set<MaterialState> states) {
+          if (states.contains(MaterialState.pressed)) {
+            return colorScheme.primary.withOpacity(0.8); // 버튼이 눌렸을 때의 배경색
+          } else if (states.contains(MaterialState.hovered)) {
+            return colorScheme.primary.withOpacity(0.8); // 버튼에 마우스가 올려졌을 때의 배경색
+          } else {
+            return colorScheme.primary; // 기본 배경색 (colorScheme.primary 사용)
+          }
+        },
+      ),
+      // 버튼 텍스트 색상 설정
+      foregroundColor: MaterialStateProperty.all(Colors.white),
+      // 버튼 모양 설정
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0), // 원하는 radius 값을 지정
+        ),
+      ),
+      elevation: MaterialStateProperty.all(5),
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      // 버튼 배경색 설정
+      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+        (Set<MaterialState> states) {
+          if (states.contains(MaterialState.pressed)) {
+            return Colors.white70; // 버튼이 눌렸을 때의 배경색
+          } else if (states.contains(MaterialState.hovered)) {
+            return Colors.white70; // 버튼에 마우스가 올려졌을 때의 배경색
+          } else {
+            return Colors.white; // 기본 배경색 (colorScheme.primary 사용)
+          }
+        },
+      ),
+      // 버튼 텍스트 색상 설정
+      foregroundColor: MaterialStateProperty.all(Colors.black87),
+      // 버튼 모양 설정
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0), // 원하는 radius 값을 지정
+        ),
+      ),
+      elevation: MaterialStateProperty.all(1.5),
+    ),
+  ),
+
+  // TextField 스타일 설정
+  inputDecorationTheme: InputDecorationTheme(
+    filled: false,
+    border: OutlineInputBorder(
+      borderSide: BorderSide.none, // 테두리 없음
+      borderRadius: BorderRadius.circular(10.0),
+    ),
+    fillColor: Colors.white,
+    hintStyle: const TextStyle(
+      color: Colors.grey,
+    ),
+  ),
+  // Checkbox 테마 스타일 설정
+  checkboxTheme: CheckboxThemeData(
+    fillColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return colorScheme.primary.withOpacity(0.8); // 선택된 상태의 색상
+      } else {
+        return colorScheme.primary; // 기본 색상
+      }
+    }),
+    overlayColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.transparent; // 선택된 상태일 때 overlay 없음
+      } else {
+        return colorScheme.primary; // 선택되지 않은 상태일 때 overlay 색상
+      }
+    }),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(3.0), // 원하는 radius 값을 지정
+    ),
+  ),
+);
