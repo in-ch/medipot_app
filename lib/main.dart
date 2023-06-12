948c177a3e8f34bbaf33791e16f5830e03f9563a
in-ch <sxin2949@naver.com>
2023-06-12 22:51:07 +0900

    build: env 파일 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+
 import 'package:medipot_app/app/pages/pages.dart';
 import 'package:medipot_app/app/style/theme.dart';
 import 'package:medipot_app/data/models/models.dart';
 
-void main() async {
-  // GetX 초기화
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Firebase 앱 초기화
+  await Firebase.initializeApp();
   DynamicLink().setup();
+
+  //화면 회전 막는 기능
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  await dotenv.load(fileName: '.env');
+
   runApp(const MyApp());
 }
 
