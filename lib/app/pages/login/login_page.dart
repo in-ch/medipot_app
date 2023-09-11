9fb187be211b81ad34176d8e47f0074fca27aab8
in-ch <sxin2949@naver.com>
2023-09-11 22:00:27 +0900

    fix(assets): kakao_logo asset linking 에러 수정

--- lib/app/pages/login/login_page.dart
+++ lib/app/pages/login/login_page.dart
@@ -16,37 +16,33 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             Expanded(
               flex: 4,
-              child: Container(
-                child: Center(
-                  child: Image.asset(
-                    'assets/image/logo.png',
-                    width: 200,
-                    height: 200,
-                  ),
+              child: Center(
+                child: Image.asset(
+                  'assets/image/logo.png',
+                  width: 200,
+                  height: 200,
                 ),
               ),
             ),
             Expanded(
-              flex: 1, // 1/3 높이
-              child: Container(
-                child: Center(
-                  child: SizedBox(
-                    width: 0.8 * MediaQuery.of(context).size.width,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // 버튼 클릭 시 수행할 동작 추가
-                      },
-                      child: Row(
-                        children: [
-                          Image.asset(
-                            'assets/image/kakao_logo.png',
-                            width: 10,
-                            height: 10,
-                          ),
-                          const Text("카카오로 로그인"),
-                        ],
-                      ),
+              flex: 1,
+              child: Center(
+                child: SizedBox(
+                  width: 0.8 * MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 버튼 클릭 시 수행할 동작 추가
+                    },
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/image/kakao_logo.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                        const Text("카카오로 로그인"),
+                      ],
                     ),
                   ),
                 ),
