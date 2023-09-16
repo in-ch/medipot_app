82e22494a70f06ab543b379d04a5ef316b7d85e8
in-ch <sxin2949@naver.com>
2023-09-17 01:05:14 +0900

    feat: webview 추가

--- lib/app/pages/home/sub/map_sub_page.dart
+++ lib/app/pages/home/sub/map_sub_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MapSubPage extends StatefulWidget {
   const MapSubPage({Key? key}) : super(key: key);
@@ -8,20 +11,45 @@ class MapSubPage extends StatefulWidget {
 }
 
 class MapSubPageState extends State<MapSubPage> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(
+          'https://medi-pots.com/webview/map?isIos=$Platform.isAndroid'));
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Column(
-      children: [
-        Text("MAP"),
-        Text("MAP"),
-        Text("MAP"),
-        Text("MAP"),
-        Text("MAP"),
-        Text("MAP"),
-        Text("MAP"),
-        Text("MAP")
-      ],
-    ));
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(child: WebViewWidget(controller: controller)),
+          ),
+        ],
+      ),
+    );
   }
 }
