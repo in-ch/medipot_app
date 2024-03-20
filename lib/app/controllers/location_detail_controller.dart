2dd84d72a27837cba76a7d62af74ce698a77dffa
in-ch <sxin2949@naver.com>
2024-03-20 23:49:29 +0900

    feat: 휴대전화 인증 확인 bottom 모달 추가 (#8)

--- lib/app/controllers/location_detail_controller.dart
+++ lib/app/controllers/location_detail_controller.dart
@@ -3,7 +3,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:medipot_app/app/routes/routes.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -61,13 +60,12 @@ class LocationDetailController extends GetxController {
       final response = await UserService.getMe();
       final data = response.data;
       if (data.phone != "") {
-        Get.toNamed(Routes.phone);
-        // showDialog(
-        //   context: context,
-        //   builder: (BuildContext context) {
-        //     return const PhoneValidationModal();
-        //   },
-        // );
+        showModalBottomSheet<void>(
+          context: context,
+          builder: (BuildContext context) {
+            return const PhoneValidationModal();
+          },
+        );
       } else {
         showDialog(
           context: context,
