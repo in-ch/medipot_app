03bcf2af60889de80754c805bd0c9993c30f7f98
in-ch <sxin2949@naver.com>
2024-08-11 20:29:25 +0900

    improvement: 뒤로가기 두번되는 버그 수정

--- lib/app/pages/careerDetail/career_detail_page.dart
+++ lib/app/pages/careerDetail/career_detail_page.dart
@@ -7,40 +7,44 @@ import 'package:docspot_app/app/views/views.dart';
 import 'package:docspot_app/app/utils/utils.dart';
 
 class CareerDetailPage extends GetView<CareerDetailController> {
-  const CareerDetailPage({Key? key}) : super(key: key);
+  const CareerDetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Theme(
         data: appTheme,
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Obx(() {
-            if (controller.isLoading.value) {
-              return const Center(child: CircularProgressIndicator());
-            } else {
-              return Stack(
-                fit: StackFit.expand,
-                children: [
-                  CareerDetailSlider(imageUrls: controller.career.imgs),
-                  CareerCardHeader(
-                    hospitalName: controller.career.hospital.name,
-                  ),
-                  CareerInfoCard(
-                    career: controller.career,
-                  ),
-                  CareerCardBottom(
-                      careerNo: controller.career.no,
-                      careerPhone: controller.career.contactPhone,
-                      isExpired:
-                          calculateDday(controller.career.deadline) == '마감'
-                              ? true
-                              : false,
-                      careerLink: controller.career.link)
-                ],
-              );
-            }
-          }),
+        child: PopScope(
+          canPop: true,
+          onPopInvoked: (_) async {},
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Obx(() {
+              if (controller.isLoading.value) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    CareerDetailSlider(imageUrls: controller.career.imgs),
+                    CareerCardHeader(
+                      hospitalName: controller.career.hospital.name,
+                    ),
+                    CareerInfoCard(
+                      career: controller.career,
+                    ),
+                    CareerCardBottom(
+                        careerNo: controller.career.no,
+                        careerPhone: controller.career.contactPhone,
+                        isExpired:
+                            calculateDday(controller.career.deadline) == '마감'
+                                ? true
+                                : false,
+                        careerLink: controller.career.link)
+                  ],
+                );
+              }
+            }),
+          ),
         ));
   }
 }
--- lib/app/pages/careerList/career_list_page.dart
+++ lib/app/pages/careerList/career_list_page.dart
@@ -13,156 +13,161 @@ class CareerListPage extends GetView<CareerListController> {
   Widget build(BuildContext context) {
     return Theme(
         data: appTheme,
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            scrolledUnderElevation: 0,
-            iconTheme: Theme.of(context).iconTheme,
-            centerTitle: false,
+        child: PopScope(
+          canPop: true,
+          onPopInvoked: (_) async {},
+          child: Scaffold(
             backgroundColor: Colors.white,
-            elevation: 0,
-            leadingWidth: 54,
-            leading: const Align(
-              alignment: Alignment.centerRight,
-              child: CustomBackButton(),
+            appBar: AppBar(
+              scrolledUnderElevation: 0,
+              iconTheme: Theme.of(context).iconTheme,
+              centerTitle: false,
+              backgroundColor: Colors.white,
+              elevation: 0,
+              leadingWidth: 54,
+              leading: const Align(
+                alignment: Alignment.centerRight,
+                child: CustomBackButton(),
+              ),
+              title: Obx(() => Text(
+                  controller.keywordValue.value != ""
+                      ? '${controller.keywordValue.value}.zip'
+                      : '초빙정보 찾기',
+                  style: Theme.of(context).textTheme.headlineSmall)),
             ),
-            title: Obx(() => Text(
-                controller.keywordValue.value != ""
-                    ? '${controller.keywordValue.value}.zip'
-                    : '초빙정보 찾기',
-                style: Theme.of(context).textTheme.headlineSmall)),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet<void>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const SelectLocationAndDepartmentModal(
-                              initTabIndex: 0,
-                            );
-                          },
-                        );
-                      },
-                      child: Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black26),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 0),
-                        child: Obx(() => Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(controller.locationValue.value,
-                                    style:
-                                        const TextStyle(color: Colors.black)),
-                                const SizedBox(width: 5),
-                                const Icon(Icons.arrow_drop_down,
-                                    size: 20, color: Colors.black38)
-                              ],
-                            )),
-                      ),
-                    ),
-                    const SizedBox(width: 15),
-                    GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet<void>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const SelectLocationAndDepartmentModal(
-                              initTabIndex: 1,
-                            );
-                          },
-                        );
-                      },
-                      child: Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black26),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 0),
-                        child: Obx(() => Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(controller.departmentValue.value,
-                                    style:
-                                        const TextStyle(color: Colors.black)),
-                                const SizedBox(width: 5),
-                                const Icon(Icons.arrow_drop_down,
-                                    size: 20, color: Colors.black38)
-                              ],
-                            )),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                Obx(
-                  () => controller.isLoading.value
-                      ? const CircularProgressIndicator()
-                      : Expanded(
-                          child: Container(
-                          color: colorScheme.surface,
-                          height: double.infinity,
-                          child: PagedGridView<int, dynamic>(
-                            pagingController: controller.pagingController,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              mainAxisSpacing: 15.0,
-                              crossAxisSpacing: 8.0,
-                              childAspectRatio: 1.0,
-                            ),
-                            builderDelegate: PagedChildBuilderDelegate<dynamic>(
-                              firstPageProgressIndicatorBuilder: (context) =>
-                                  Container(
-                                alignment: Alignment.center,
-                                width: 50,
-                                height: 50,
-                                child: CircularProgressIndicator(
-                                  valueColor: AlwaysStoppedAnimation<Color>(
-                                      colorScheme.primary),
-                                ),
-                              ),
-                              noItemsFoundIndicatorBuilder: (context) =>
-                                  NoDatas(text: '검색 결과가 없습니다.'),
-                              firstPageErrorIndicatorBuilder: (context) =>
-                                  NoDatas(text: '검색 결과가 없습니다.'),
-                              newPageProgressIndicatorBuilder: (context) =>
-                                  Container(
-                                alignment: Alignment.center,
-                                width: 50,
-                                height: 50,
-                                child: CircularProgressIndicator(
-                                  valueColor: AlwaysStoppedAnimation<Color>(
-                                      colorScheme.primary),
-                                ),
-                              ),
-                              itemBuilder: (context, item, index) {
-                                return CareerItem(
-                                  no: item.no,
-                                  img: item.imgs[0],
-                                  company: item.hospital.name,
-                                  position: item.title,
-                                  deadline: item.deadline,
-                                );
-                              },
-                            ),
+            body: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const SelectLocationAndDepartmentModal(
+                                initTabIndex: 0,
+                              );
+                            },
+                          );
+                        },
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(20.0),
                           ),
-                        )),
-                )
-              ],
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 0),
+                          child: Obx(() => Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(controller.locationValue.value,
+                                      style:
+                                          const TextStyle(color: Colors.black)),
+                                  const SizedBox(width: 5),
+                                  const Icon(Icons.arrow_drop_down,
+                                      size: 20, color: Colors.black38)
+                                ],
+                              )),
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+                      GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const SelectLocationAndDepartmentModal(
+                                initTabIndex: 1,
+                              );
+                            },
+                          );
+                        },
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 0),
+                          child: Obx(() => Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(controller.departmentValue.value,
+                                      style:
+                                          const TextStyle(color: Colors.black)),
+                                  const SizedBox(width: 5),
+                                  const Icon(Icons.arrow_drop_down,
+                                      size: 20, color: Colors.black38)
+                                ],
+                              )),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  Obx(
+                    () => controller.isLoading.value
+                        ? const CircularProgressIndicator()
+                        : Expanded(
+                            child: Container(
+                            color: colorScheme.surface,
+                            height: double.infinity,
+                            child: PagedGridView<int, dynamic>(
+                              pagingController: controller.pagingController,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 15.0,
+                                crossAxisSpacing: 8.0,
+                                childAspectRatio: 1.0,
+                              ),
+                              builderDelegate:
+                                  PagedChildBuilderDelegate<dynamic>(
+                                firstPageProgressIndicatorBuilder: (context) =>
+                                    Container(
+                                  alignment: Alignment.center,
+                                  width: 50,
+                                  height: 50,
+                                  child: CircularProgressIndicator(
+                                    valueColor: AlwaysStoppedAnimation<Color>(
+                                        colorScheme.primary),
+                                  ),
+                                ),
+                                noItemsFoundIndicatorBuilder: (context) =>
+                                    NoDatas(text: '검색 결과가 없습니다.'),
+                                firstPageErrorIndicatorBuilder: (context) =>
+                                    NoDatas(text: '검색 결과가 없습니다.'),
+                                newPageProgressIndicatorBuilder: (context) =>
+                                    Container(
+                                  alignment: Alignment.center,
+                                  width: 50,
+                                  height: 50,
+                                  child: CircularProgressIndicator(
+                                    valueColor: AlwaysStoppedAnimation<Color>(
+                                        colorScheme.primary),
+                                  ),
+                                ),
+                                itemBuilder: (context, item, index) {
+                                  return CareerItem(
+                                    no: item.no,
+                                    img: item.imgs[0],
+                                    company: item.hospital.name,
+                                    position: item.title,
+                                    deadline: item.deadline,
+                                  );
+                                },
+                              ),
+                            ),
+                          )),
+                  )
+                ],
+              ),
             ),
           ),
         ));
