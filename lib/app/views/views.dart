e17cc640b576dc7d69bd6f97e5f39e16b80d81b3
in-ch <sxin2949@naver.com>
2024-09-13 18:44:36 +0900

    안읽은 알림 api 연동 (#36)

--- lib/app/views/views.dart
+++ lib/app/views/views.dart
@@ -64,11 +64,13 @@ export './career/career_card_bottom.dart';
 export './career/career_card_header.dart';
 export './career/career_company_info.dart';
 export './career/banner_slider.dart';
+export 'career/show_new_alarms.dart';
 
 export './search/search_keyword.dart';
 export './search/search_rank.dart';
 
 export './alram/alarm_item.dart';
+export './alram/go_to_alarm.dart';
 
 export './game/roulette_painter.dart';
 export './game/triangle_painter.dart';
--- lib/services/alarm_service.dart
+++ lib/services/alarm_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -16,6 +17,46 @@ class AlarmService {
       return jsonDecode(response.body);
     } else {
       throw Exception('Failed to getAlarms fetch list');
+    }
+  }
+
+  static Future<bool> isUnReadAlarm() async {
+    try {
+      String apiServer = dotenv.get("API_SERVER");
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? accessToken = prefs.getString('accessToken');
+      final headers = {'Authorization': 'Bearer $accessToken'};
+      final url = Uri.parse('$apiServer/alarm/unread');
+      final response = await http.get(url, headers: headers);
+      final responseData = jsonDecode(response.body);
+      if (responseData['statusCode'] == 200) {
+        return responseData['data'] ?? false;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      debugPrint("Error in isUnReadAlarm: $error");
+      return false;
+    }
+  }
+
+  static Future<bool> setAlarmAllRead() async {
+    try {
+      String apiServer = dotenv.get("API_SERVER");
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? accessToken = prefs.getString('accessToken');
+      final headers = {'Authorization': 'Bearer $accessToken'};
+      final url = Uri.parse('$apiServer/alarm/read');
+      final response = await http.put(url, headers: headers);
+      final responseData = jsonDecode(response.body);
+      if (responseData['statusCode'] == 200) {
+        return responseData['data'] ?? false;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      debugPrint("Error in isUnReadAlarm: $error");
+      return false;
     }
   }
 }
