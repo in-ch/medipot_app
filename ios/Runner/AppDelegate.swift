2eb387741777a6a2d69e111790a8e6e40bd0566a
in-ch <sxin2949@naver.com>
2024-11-25 22:20:03 +0900

    feat: 스크린샷 방지 기능 추가

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,6 +13,35 @@ import GoogleMaps
     }
     GeneratedPluginRegistrant.register(with: self)
     GMSServices.provideAPIKey("AIzaSyB0EmUeUH38p_PUfV-jIZZlbuAexa6RoxA")
+
+    NotificationCenter.default.addObserver(self, selector: #selector(handleScreenCapture), name: UIScreen.capturedDidChangeNotification, object: nil)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  @objc func handleScreenCapture() {
+    if UIScreen.main.isCaptured {
+        print("스크린샷이 캡처되었습니다!")
+
+        hideSensitiveInformation()
+    } else {
+        restoreSensitiveInformation()
+    }
+  }
+
+  func hideSensitiveInformation() {
+    if let rootViewController = window?.rootViewController {
+        let overlayView = UIView(frame: rootViewController.view.bounds)
+        overlayView.backgroundColor = UIColor.black
+        overlayView.tag = 999
+        rootViewController.view.addSubview(overlayView)
+    }
+  }
+
+  func restoreSensitiveInformation() {
+    if let rootViewController = window?.rootViewController {
+        if let overlayView = rootViewController.view.viewWithTag(999) {
+            overlayView.removeFromSuperview()  
+        }
+    }
   }
 }
