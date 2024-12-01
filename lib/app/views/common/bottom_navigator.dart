5be700e374648c425a452f34ff3b725ea6ae35b6
in-ch <sxin2949@naver.com>
2024-12-01 22:29:22 +0900

    style: 바텀 네비게이션 아이콘 크기 확장

--- lib/app/views/common/bottom_navigator.dart
+++ lib/app/views/common/bottom_navigator.dart
@@ -41,27 +41,30 @@ class BottomNavigationWidgetState extends State<BottomNavigation> {
   Widget _buildNavItem(IconData icon, String label, int index) {
     return GestureDetector(
       onTap: () => widget.onTabTapped(index),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Icon(
-            icon,
-            color: widget.currentIndex == index
-                ? colorScheme.primary
-                : Colors.black26,
-          ),
-          const SizedBox(height: 6),
-          Text(
-            label,
-            style: TextStyle(
+      child: Container(
+        padding: const EdgeInsets.only(left: 10, right: 10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(
+              icon,
               color: widget.currentIndex == index
                   ? colorScheme.primary
                   : Colors.black26,
-              fontSize: 12,
-              fontWeight: FontWeight.w200,
             ),
-          ),
-        ],
+            const SizedBox(height: 6),
+            Text(
+              label,
+              style: TextStyle(
+                color: widget.currentIndex == index
+                    ? colorScheme.primary
+                    : Colors.black26,
+                fontSize: 12,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -70,59 +73,62 @@ class BottomNavigationWidgetState extends State<BottomNavigation> {
       IconData icon, String label, int index, String badgeText, String route) {
     return GestureDetector(
       onTap: () => Get.toNamed(route),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned(
-                child: SizedBox(
-                  width: 60,
-                  height: 30,
-                  child: Icon(
-                    icon,
-                    color: widget.currentIndex == index
-                        ? colorScheme.primary
-                        : Colors.black26,
+      child: Container(
+        padding: const EdgeInsets.only(left: 10, right: 10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  child: SizedBox(
+                    width: 60,
+                    height: 30,
+                    child: Icon(
+                      icon,
+                      color: widget.currentIndex == index
+                          ? colorScheme.primary
+                          : Colors.black26,
+                    ),
                   ),
                 ),
-              ),
-              badgeText == "0"
-                  ? Container()
-                  : Positioned(
-                      right: badgeText.length > 1 ? 0 : 5,
-                      top: 0,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 7, vertical: 2),
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Text(
-                          badgeText,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 11,
+                badgeText == "0"
+                    ? Container()
+                    : Positioned(
+                        right: badgeText.length > 1 ? 0 : 5,
+                        top: 0,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 7, vertical: 2),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Text(
+                            badgeText,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 11,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-            ],
-          ),
-          const SizedBox(height: 6),
-          Text(
-            label,
-            style: TextStyle(
-              color: widget.currentIndex == index
-                  ? colorScheme.primary
-                  : Colors.black26,
-              fontSize: 12,
-              fontWeight: FontWeight.w200,
+              ],
             ),
-          ),
-        ],
+            const SizedBox(height: 6),
+            Text(
+              label,
+              style: TextStyle(
+                color: widget.currentIndex == index
+                    ? colorScheme.primary
+                    : Colors.black26,
+                fontSize: 12,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
