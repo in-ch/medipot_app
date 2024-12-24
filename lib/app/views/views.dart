69b68d6af0a6d46f1766d7b36d43da9dac911528
in-ch <sxin2949@naver.com>
2024-12-24 14:33:46 +0900

    feat(chat): 로그인 요청 모달창 삭제 -> 로그인 페이지 이동으로 대체

--- lib/app/views/views.dart
+++ lib/app/views/views.dart
@@ -35,7 +35,6 @@ export './modals/please_login_at_main_modal.dart';
 export './modals/please_input_email_modal.dart';
 export './modals/taro_result_modal.dart';
 export './modals/message_clip_board_modal.dart';
-export './modals/login_modal.dart';
 export './modals/grant_modal.dart';
 
 export './small_circle.dart';
